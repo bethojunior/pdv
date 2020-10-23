fdefe526f349c224733bd2e54260a11c6bfb7ddd
Betho Jr <hgomescostajr@gmail.com>
2020-10-23 18:12:29 -0300

    teste

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,14 @@
 @extends('adminlte::auth.login')
+<link rel="manifest" href="{{ asset('manisfest.json') }}">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="msapplication-starturl" content="/">
+<link rel="stylesheet" href="https://loja704.com.br/css/dashboard/home.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script>
+    function saveBeforeInstallPromptEvent(evt) {
+        deferredInstallPrompt = evt;
+        deferredInstallPrompt.prompt();
+    }
+    window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+</script>
