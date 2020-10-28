1a1ce57898495ee65a3647ac8e1269281cd4a673
bethojunior <hgomescostajr@gmail.com>
2020-10-28 15:28:47 -0300

    service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,9 +1,42 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            console.log('ServiceWorker registration :', registration.scope);
-        }).catch(function (error) {
-            console.log('ServiceWorker registration failed:', errror);
-        });
-    });
-}
+var staticCacheName = "pwa-v" + new Date().getTime();
+var filesToCache = [
+    '/'
+];
+
+// Cache on install
+self.addEventListener("install", event => {
+    this.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    )
+});
+
+// Clear cache on activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith("pwa-")))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// Serve from Cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('offline');
+            })
+    )
+});
